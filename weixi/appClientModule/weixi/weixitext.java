2fa6e400407563f5a8749ac4a58bf4738acdd9ab
weixihub <326214154@qq.com>
2020-06-09 15:28:57 +0800

    上传文件

---
+++ weixi/appClientModule/weixi/weixitext.java
@@ -0,0 +1,19 @@
+package weixi;
+
+public class weixitext {
+	
+	public void addUser(int name,String username,String password) {
+		//Ìí¼Ó
+		
+	}
+	
+	public void deleteuser(int name) {
+		//É¾³ý
+	}
+	
+	public void updateUser(int name,String username,String password) {
+		//ÐÞ¸Ä
+		
+	}
+
+}
